5834657076dfa779100a2fda000b343d4c8918bb
Masatoshi Kubode <masatoshi@wantedly.com>
2021-05-13 20:32:55 +0900

    Suppress warnings

--- reaktor/src/commonTest/kotlin/com/github/kubode/reaktor/BaseTest.kt
+++ reaktor/src/commonTest/kotlin/com/github/kubode/reaktor/BaseTest.kt
@@ -3,9 +3,9 @@ package com.github.kubode.reaktor
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.runBlocking
 
-expect abstract class BaseTest() {
-}
+expect abstract class BaseTest()
 
+@Suppress("unused") // Because it's impossible to describe functions in the expect class.
 fun BaseTest.runTest(block: suspend CoroutineScope.() -> Unit) {
     runBlocking {
         block()
