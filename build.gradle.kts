d6860e3a2482bbc7cd21e371c7e0fb50c2bc2b45
Masatoshi Kubode <masatoshi@wantedly.com>
2021-05-13 21:23:46 +0900

    Integrate with ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,13 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.0")
         classpath("com.android.tools.build:gradle:4.2.0")
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:10.0.0")
     }
 }
 
 allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
     repositories {
         google()
         mavenCentral()
