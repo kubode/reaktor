2e041d0a44d945a9fd1eb7227d7f6d1873fc5e1b
Masatoshi Kubode <masatoshi@wantedly.com>
2021-05-17 12:28:59 +0900

    Update dependencies

--- reaktor/build.gradle.kts
+++ reaktor/build.gradle.kts
@@ -10,24 +10,24 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0-RC-native-mt")
+                api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0-native-mt")
             }
         }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
                 implementation("io.kotest:kotest-assertions-core:4.5.0")
-                implementation("app.cash.turbine:turbine:0.5.0-rc1")
+                implementation("app.cash.turbine:turbine:0.5.0")
             }
         }
         val androidMain by getting {
             dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0-RC-native-mt")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0-native-mt")
             }
         }
         val androidTest by getting {
             dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.0-RC-native-mt")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.0-native-mt")
                 implementation("androidx.test:runner:1.3.0")
                 implementation("androidx.test:rules:1.3.0")
                 implementation("androidx.test.ext:junit:1.1.2")
