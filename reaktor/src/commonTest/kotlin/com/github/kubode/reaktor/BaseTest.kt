24e5f9858bd702b281887d0735ab3ff7c5f1c832
Masatoshi Kubode <masatoshi@wantedly.com>
2021-05-18 22:04:37 +0900

    Add timeout to runTest

--- reaktor/src/commonTest/kotlin/com/github/kubode/reaktor/BaseTest.kt
+++ reaktor/src/commonTest/kotlin/com/github/kubode/reaktor/BaseTest.kt
@@ -2,12 +2,15 @@ package com.github.kubode.reaktor
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
 
 expect abstract class BaseTest()
 
 @Suppress("unused") // Because it's impossible to describe functions in the expect class.
-fun BaseTest.runTest(block: suspend CoroutineScope.() -> Unit) {
+fun BaseTest.runTest(timeoutMillis: Long = 1000, block: suspend CoroutineScope.() -> Unit) {
     runBlocking {
-        block()
+        withTimeout(timeoutMillis) {
+            block()
+        }
     }
 }
