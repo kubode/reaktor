1b8d0da26eb5c4376515de72366ac6643d1d700f
Masatoshi Kubode <swi.masatoshi@gmail.com>
2021-07-19 22:18:37 +0900

    Kotlin 1.5.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.0")
+        classpath(kotlin("gradle-plugin", "1.5.21"))
         classpath("com.android.tools.build:gradle:7.1.0-alpha03")
         classpath("org.jlleitschuh.gradle:ktlint-gradle:10.0.0")
     }
