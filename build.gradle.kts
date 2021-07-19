332d90356065c1dab35a9772e44238ec4fc68e31
Masatoshi Kubode <swi.masatoshi@gmail.com>
2021-07-19 23:36:31 +0900

    Kotlin 1.5.30-RC

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,12 @@ buildscript {
         gradlePluginPortal()
         google()
         mavenCentral()
+        maven {
+            url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/")
+        }
     }
     dependencies {
-        classpath(kotlin("gradle-plugin", "1.5.21"))
+        classpath(kotlin("gradle-plugin", "1.5.30-RC-161"))
         classpath("com.android.tools.build:gradle:7.1.0-alpha03")
         classpath("org.jlleitschuh.gradle:ktlint-gradle:10.0.0")
     }
@@ -17,6 +20,9 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/")
+        }
     }
     group = "com.github.kubode.reaktor"
     version = "0.1.0"
