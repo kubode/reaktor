2cd485dc9562bde1d96b048e5e888fd4f876eb51
Masatoshi Kubode <swi.masatoshi@gmail.com>
2021-07-19 22:17:10 +0900

    AGP 7.1.0-alpha03

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.0")
-        classpath("com.android.tools.build:gradle:4.2.1")
+        classpath("com.android.tools.build:gradle:7.1.0-alpha03")
         classpath("org.jlleitschuh.gradle:ktlint-gradle:10.0.0")
     }
 }
